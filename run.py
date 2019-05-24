3d6f0e0aa0a069d20f836517f9b803f55b357f1f
anuragcp <anudeveloper777@gmail.com>
2019-05-24 23:45:15 +0530

    made lable_image file

--- run.py
+++ run.py
@@ -1,7 +1,29 @@
 import RPi.GPIO as GPIO
 import time
+import label_image
+from picamera import PiCamera
+from PIL import Image
 
-GPIO.setmode(GPIO.BOARD)
+img = []
+#if utrasonic detects a object
+stream = BytesIO()
+camera = PiCamera()
+camera.start_preview()
+sleep(2)
+camera.capture(stream, format='jpeg')
+stream.seek(0)
+img = Image.open(stream)
+
+graph = load_graph(model_file)
+  t = read_tensor_from_image_file(file_name,
+                                  input_height=input_height,
+                                  input_width=input_width,
+                                  input_mean=input_mean,
+                                  input_std=input_std)
+
+
+
+"""GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.OUT)
 p = GPIO.PWM(12, 50)
 p.start(7.5)
@@ -16,4 +38,4 @@ try:
         time.sleep(1) # sleep 1 second 
 except KeyboardInterrupt:
     p.stop()
-    GPIO.cleanup()
+    GPIO.cleanup()"""

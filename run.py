a13960caebcf1938b8cba021c5d481717c3bcb15
anuragcp <anudeveloper777@gmail.com>
2019-05-24 23:49:12 +0530

    ready to test prediction

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from picamera import PiCamera
 from PIL import Image
 
 img = []
+model_file = "./model/retrained_graph.pb"
 #if utrasonic detects a object
 stream = BytesIO()
 camera = PiCamera()
@@ -15,12 +16,29 @@ stream.seek(0)
 img = Image.open(stream)
 
 graph = load_graph(model_file)
-  t = read_tensor_from_image_file(file_name,
+  t = read_tensor_from_image_file(file_name = img,
                                   input_height=input_height,
                                   input_width=input_width,
                                   input_mean=input_mean,
                                   input_std=input_std)
+input_name = "import/" + input_layer
+output_name = "import/" + output_layer
+input_operation = graph.get_operation_by_name(input_name);
+output_operation = graph.get_operation_by_name(output_name);
+with tf.Session(graph=graph) as sess:
+	start = time.time()
+    results = sess.run(output_operation.outputs[0],
+                      {input_operation.outputs[0]: t})
+    end=time.time()
+results = np.squeeze(results)
 
+top_k = results.argsort()[-5:][::-1]
+labels = load_labels(label_file)
+
+print('\nEvaluation time (1-image): {:.3f}s\n'.format(end-start))
+template = "{} (score={:0.5f})"
+for i in top_k:
+	print(template.format(labels[i], results[i]))
 
 
 """GPIO.setmode(GPIO.BOARD)
